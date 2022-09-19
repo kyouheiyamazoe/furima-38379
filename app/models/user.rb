d19450fbf2989a30f8114a9f722b9c7264d629ee
kyouheiyamazoe <110809401+kyouheiyamazoe@users.noreply.github.com>
2022-09-19 22:57:00 +0900

    バリデーションの記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :purchases
+
+  validates :nickname,        presence: true
+  validates :password
+  validates :lastt_name,      presence: true
+  validates :first_name,      presence: true
+  validates :last_name_kana,  presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday,        presence: true
 end
