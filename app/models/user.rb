ec2bf949cef37eb6b7579d8c430576801f71eac5
kyouheiyamazoe <110809401+kyouheiyamazoe@users.noreply.github.com>
2022-09-20 14:13:00 +0900

    トップページ表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :purchases
 
   validates :nickname,        presence: true
-  validates :password
+  validate :password
   validates :lastt_name,      presence: true
   validates :first_name,      presence: true
   validates :last_name_kana,  presence: true
